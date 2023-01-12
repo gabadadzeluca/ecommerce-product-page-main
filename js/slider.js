202eced05a0647421dd82fdf9cc965c54ca08948
Luca <gabadadzeluca@gmail.com>
2023-01-12 14:35:52 +0400

    rewrite as a function to use for desktop page

--- js/slider.js
+++ js/slider.js
@@ -7,40 +7,65 @@ const sliderImgs = document.querySelectorAll('.slider img');
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
 
-let current = 1;
-let size = sliderImgs[0].clientWidth;
 
-// set the first image
-slider.style.transform = 'translateX(' + (-size) + 'px)';
+// mobile slider
+sliderFunc(slider, arrowLeft, arrowRight);
+
+function sliderFunc(slider, arrowLeft, arrowRight){
+    const sliderImgs = slider.querySelectorAll('img');
+
+    let current = 1;
+    let size = sliderImgs[0].clientWidth;
+    
+
+    // set the first image
+    slider.style.transform = 'translateX(' + (-size) + 'px)';
+    
+    
+    arrowRight.addEventListener('click', ()=>{
+        if(current >= 5) return;
+        slider.style.transition = 'transform 0.4s ease-in-out';
+        current++;
+        slider.style.transform = `translateX(${-size*current}px)`;
+    });
+    
+    arrowLeft.addEventListener('click', ()=>{
+        if(current <= 0) return;
+        slider.style.transition = 'transform 0.4s ease-in-out';
+        current--;
+        slider.style.transform = `translateX(${-size * current}px)`;
+    });
+    
+    slider.addEventListener('transitionend', () => {
+        if (sliderImgs[current].id === 'first-clone') {
+            current = 1;
+            slider.style.transition = 'none';
+            slider.style.transform = `translateX(${-size * current}px)`;
+        }
+        if (sliderImgs[current].id === 'last-clone') {
+            current = sliderImgs.length - 2;
+            slider.style.transition = 'none';
+            slider.style.transform = `translateX(${-size * current}px)`;
+        }
+      });
+    
+      window.onresize = function() {
+        location.reload();
+    };
+    
+}
 
 
-arrowRight.addEventListener('click', ()=>{
-    if(current >= 5) return;
-    slider.style.transition = 'transform 0.4s ease-in-out';
-    current++;
-    slider.style.transform = `translateX(${-size*current}px)`;
+const mainImg = document.querySelector('.main-img img');
+const smallImgs = document.querySelectorAll('.small-imgs img');
+
+smallImgs.forEach(img=>{
+    img.addEventListener('click', displaySlider)
 });
 
-arrowLeft.addEventListener('click', ()=>{
-    if(current <= 0) return;
-    slider.style.transition = 'transform 0.4s ease-in-out';
-    current--;
-    slider.style.transform = `translateX(${-size * current}px)`;
-});
+mainImg.addEventListener('click', displaySlider);
+function displaySlider(){
+    console.log(this.id);
+    // display slider for desktop
 
-slider.addEventListener('transitionend', () => {
-    if (sliderImgs[current].id === 'first-clone') {
-        current = 1;
-        slider.style.transition = 'none';
-        slider.style.transform = `translateX(${-size * current}px)`;
-    }
-    if (sliderImgs[current].id === 'last-clone') {
-        current = sliderImgs.length - 2;
-        slider.style.transition = 'none';
-        slider.style.transform = `translateX(${-size * current}px)`;
-    }
-  });
-
-  window.onresize = function() {
-    location.reload();
-};
+}
